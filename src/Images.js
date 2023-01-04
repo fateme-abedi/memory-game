e054befbf22a84584c002a6b5f617d7d96a5f4e7
fatemeabedi <abedifateme1378@gmail.com>
2023-01-04 22:31:50 +0330

    export images

---
+++ src/Images.js
@@ -0,0 +1,21 @@
+import Image1 from './images/01.jpg'
+import Image2 from './images/02.jpg'
+import Image3 from './images/03.jpg'
+import Image4 from './images/04.jpg'
+import Image5 from './images/05.jpg'
+import Image6 from './images/06.jpg'
+import Image7 from './images/07.jpg'
+import Image8 from './images/08.jpg'
+import Image9 from './images/09.jpg'
+import Image10 from './images/10.jpg'
+import Image11 from './images/11.jpg'
+import Image12 from './images/12.jpg'
+import Image13 from './images/13.jpg'
+import Image14 from './images/14.jpg'
+import Image15 from './images/15.jpg'
+import Image16 from './images/16.jpg'
+import Image17 from './images/17.jpg'
+import Image18 from './images/18.jpg'
+
+export default [Image1,Image2,Image3,Image4,Image5,Image6,Image7,Image8,Image9,Image10,Image11,Image12,
+Image13,Image14,Image15,Image16,Image17,Image18]
